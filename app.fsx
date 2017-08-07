d33b5adfb102a41fe3a45d15cc93b401dfba496f
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-07 18:39:33 -0400

    added remaining SVG's, moved to root instead of 1 mega file

--- app.fsx
+++ app.fsx
@@ -1,5 +1,5 @@
-#r "./packages/Suave/lib/net40/Suave.dll"
-#r "./packages/Suave.Experimental/lib/net40/Suave.Experimental.dll"
+#load "root.fsx"
+
 open Suave
 open Suave.Cookie
 open Suave.Filters
@@ -9,12 +9,8 @@ open Suave.RequestErrors
 open Suave.Utils
 open Suave.Successful
 open Suave.Authentication
-
-let ul = tag "ul"
-let li = tag "li"
+open Root
 let nav = tag "nav"
-
-let homeSvg = rawText """<svg class="homeSvg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"> <g>	<path d="M512,296l-96-96V56h-64v80l-96-96L0,296v16h64v160h160v-96h64v96h160V312h64V296z"></path> </g> <g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>""" |> Seq.head
 
 let samplePage =
   html [] [
@@ -38,24 +34,7 @@ let samplePage =
           a "/logout" ["class", "btn btn-default"] []
         ]
         nav ["class", "main-nav"] [
-          ul [] [
-            li ["class", "nav-item"] [
-              homeSvg
-              a "/"  [] (text "Welcome")
-            ]
-            li ["class", "nav-item"] [
-              a "/careplan" [] (text "Care Plan")
-            ]
-            li ["class", "nav-item"] [
-              a "/messages" [] (text "Messages")
-            ]
-            li ["class", "nav-item"] [
-              a "/medications" [] (text "Medications")
-            ]
-            li ["class", "nav-item"] [
-              a "/forms" [] (text "Forms")
-            ]
-          ]
+          Root.getNavMenu "/"
         ]
         div ["class", "main-sidebar"] [
           p [] (text "Stevens, Joseph")
