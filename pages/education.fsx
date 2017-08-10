a737efe448c11a871d8104eb2e0496e0c5162b1c
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-10 19:36:12 -0400

    open pdf's in new tab now

--- pages/education.fsx
+++ pages/education.fsx
@@ -5,15 +5,18 @@ open Suave.Html
 open DataAccess
 
 let Home = 
+  let getPdf url str =
+    a url ["class", "list-group-item"; "target", "blank"] (text str)
+
   div ["class", "container-fluid horizontalCentered"] [
     div ["class", "row"] [
       div ["id", "MainMenu"] [
         div ["class", "list-group panel"] [
-          a "../Content/brochure_basicsofalz_low.pdf" ["class", "list-group-item"] (text "Alzheimer’s Disease")
-          a "../Content/tenwarnsignsofalzheimers.pdf" ["class", "list-group-item"] (text "10 Warning Signs of Alzheimer’s Disease")
-          a "../Content/brochure_prevention.pdf" ["class", "list-group-item"] (text "Preventing Alzheimer's Disease")
-          a "../Content/topicsheetTreatments.pdf" ["class", "list-group-item"] (text "Approved treatments for Alzheimer’s")
-          a "../Content/copdAtrisk.pdf" ["class", "list-group-item"] (text "Basics Of Chronic Obstructive Pulmonary Disease")
+          getPdf "../Content/brochureBasicsofalz_low.pdf" "Alzheimer’s Disease"
+          getPdf "../Content/tenwarnsignsofalzheimers.pdf" "10 Warning Signs of Alzheimer’s Disease"
+          getPdf "../Content/brochure_prevention.pdf" "Preventing Alzheimer's Disease"
+          getPdf "../Content/topicsheetTreatments.pdf"  "Approved treatments for Alzheimer’s"
+          getPdf "../Content/copdAtrisk.pdf" "Basics Of Chronic Obstructive Pulmonary Disease"
         ]
       ]
     ]
