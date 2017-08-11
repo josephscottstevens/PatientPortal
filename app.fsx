afd4d5e5f1bb71305ba4957670506c90f60448c9
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-11 08:56:01 -0400

    removed auth from app

--- app.fsx
+++ app.fsx
@@ -7,18 +7,17 @@ open Suave.Operators
 open Suave.RequestErrors
 open Suave.Utils
 open Suave.Successful
-open Suave.Authentication
 open Root
 
 let app = 
-    State.CookieStateStore.statefulForSession >=> choose 
-      [
-        Files.browseHome
-        path "/"            >=> OK (basePage "/" HomePage.Home)
-        path "/careplan"    >=> OK (basePage "/careplan" CarePlan.Home)
-        path "/feedback"    >=> OK (basePage "/feedback" Feedback.Home)
-        path "/medications" >=> OK (basePage "/medications" Medications.Home)
-        path "/forms"       >=> OK (basePage "/forms" Forms.Home)
-        path "/education"   >=> OK (basePage "/education" Education.Home)
-        path "/proxies"     >=> OK (basePage "/proxies" Proxies.Home)
-      ]
+  State.CookieStateStore.statefulForSession >=> choose 
+    [
+      Files.browseHome
+      path "/"            >=> OK (basePage "/" HomePage.Home)
+      path "/careplan"    >=> OK (basePage "/careplan" CarePlan.Home)
+      path "/feedback"    >=> OK (basePage "/feedback" Feedback.Home)
+      path "/medications" >=> OK (basePage "/medications" Medications.Home)
+      path "/forms"       >=> OK (basePage "/forms" Forms.Home)
+      path "/education"   >=> OK (basePage "/education" Education.Home)
+      path "/proxies"     >=> OK (basePage "/proxies" Proxies.Home)
+    ]
