b05a25412bf6e17304911dddc53e17e1a42b4fe8
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-13 16:11:51 -0400

    simple example

--- pages/Forms.fsx
+++ pages/Forms.fsx
@@ -6,28 +6,9 @@ open DataAccess
 
 let js = 
   """
-  $('#example').DataTable( {
-        initComplete: function () {
-            this.api().columns().every( function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
- 
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-        }
-    } );
+  $(document).ready(function() {
+    var table = $('#example').DataTable();
+  });
   """
 
 let data = 
