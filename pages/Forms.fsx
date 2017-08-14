8aff2d1bb9553be687367c559d7f3c7df2491846
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-13 20:06:51 -0400

    added toolbar and search

--- pages/Forms.fsx
+++ pages/Forms.fsx
@@ -9,8 +9,10 @@ let js =
   $(function() {
 	$("#Grid").ejGrid({
 		dataSource: ej.DataManager($("#Table1")),
+    toolbarSettings : { showToolbar : true, toolbarItems : ["search"] },
     allowPaging : true,
 		allowSorting : true,
+    allowSearching : true,
 		columns: ["FullName", "Phone", "HomeCity", "HomeState", "HomeZip"]
 	});
 });
