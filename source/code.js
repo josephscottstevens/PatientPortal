0ce79480909eee387d376db0469967fdcfd03359
Joseph Stevens <josephscottstevens@gmail.com>
2017-08-22 13:00:38 -0400

    adding initial js

--- source/code.js
+++ source/code.js
@@ -1,10 +1,65 @@
-$(function() {
-	$("#Grid").ejGrid({
-		dataSource: ej.DataManager($("#Table1")),
-    toolbarSettings : { showToolbar : true, toolbarItems : ["search"] },
-    allowPaging : true,
-		allowSorting : true,
-    allowSearching : true,
-		columns: ["FullName", "Phone", "HomeCity", "HomeState", "HomeZip"]
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var sourceDiv = document.getElementById(data);
+  var sourceCol = sourceDiv.style.gridColumnStart;
+  var sourceClass = "." + sourceDiv.id;
+  var targetDiv = ev.target;
+  var targetCol = targetDiv.style.gridColumnStart;
+  // Bit of a gotcha here, if you give input an ID, this will break;
+  var targetClass = (targetDiv.id != "") ? "." + targetDiv.id : "." + targetDiv.parentNode.id;
+  
+  document.querySelectorAll(sourceClass).forEach(t => t.style.gridColumnStart = targetCol);
+  document.querySelectorAll(targetClass).forEach(t => t.style.gridColumnStart = sourceCol);
+}
+
+function filterMe(e) {
+  var targetClass = "." + e.parentNode.id + "Col";
+  document.querySelectorAll(targetClass).forEach(function(t) {
+    var rowQuery = "[data-row = '" + t.parentNode.getAttribute("data-row") + "']";
+    if (t.innerText.toLowerCase().includes(e.value.toLowerCase())) {
+      t.parentNode.style.display = "";
+    } else {
+      t.parentNode.style.display = "none";
+    }
   });
-});
+}
+
+function compareNumbers(a, b) {
+  return a.value - b.value;
+}
+
+function sortMe(e) {
+  var targetClass = "." + e.parentNode.id + "Col";
+  var data = [];
+  document.querySelectorAll(targetClass).forEach(function(t) {
+    data.push({value:t.innerText, rowNum:t.parentNode.getAttribute("data-row")});
+  });
+  var sortedData = data.sort(compareNumbers);
+  sortedData.forEach(function (val, idx) {
+    var rowSelect = "[data-row = '" + val.rowNum + "']";
+    document.querySelector(rowSelect).style.gridRow = idx+2;
+  });
+}
+
+function toggleMe(e) {
+  var siblings = e.target.parentNode.parentNode.children;
+  var detailRow = Array.prototype.filter.call(siblings, function(child){
+    return child.className == "detailRow";
+  })[0];
+  if (e.target.value == ">") {
+    e.target.value = "↓";
+    detailRow.style.display = "";
+  } else {
+    e.target.value = ">";
+    detailRow.style.display = "none";
+  }
+}
